41af3912ef90eeba9aec537762bd34799e6c2633
luke <luke.edwards05@gmail.com>
2016-02-03 12:30:59 -0800

    working plugin (!)

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+const fs = require('fs')
+const path = require('path')
 const assign = require('object-assign')
 const strEsc = require('js-string-escape')
-
-const HEADER  = 'angular.module("<%= module %>"<%= standalone %>).run(["$templateCache", function($templateCache) {'
-const CONTENT = '$templateCache.put("<%= url %>","<%= contents %>");'
-const FOOTER  = '}]);'
 
 const modules = {
   requirejs: {
@@ -24,17 +22,67 @@ const modules = {
 
 const defaults = {
   standalone: false,
-  filename: 'templates.js',
+  fileBase: '',
+  fileName: 'templates.js',
   moduleName: 'templates',
   moduleSystem: null,
-  templateHeader: HEADER,
-  templateContent: CONTENT,
-  templateFooter: FOOTER,
-  templateUrl: () => false
+  templateHeader: 'angular.module("<%= module %>"<%= standalone %>).run(["$templateCache", function($templateCache) {',
+  templateContent: '$templateCache.put("<%= url %>","<%= contents %>");',
+  templateFooter: '}]);',
+  templateUrl: (url) => url
 }
 
 module.exports = function () {
-  this.ngTemplates = (options) => {
-    options = assign(defaults, options)
+  this.filter('ngTemplates', (data, options) => {
+    const opts = assign(defaults, options)
+
+    return this.unwrap(files =>
+      files.map(file => buildContent(file, opts)).join('')
+    ).then(data => {
+      data = angularWrapper(data, opts)
+      data = moduleWrapper(data, opts)
+
+      this.concat(opts.fileName)
+
+      return data
+    })
+  })
+}
+
+/**
+ * Wrap each file's contents in a $templateCache definition
+ */
+function buildContent (file, opts) {
+  const url = opts.templateUrl( path.normalize(file) )
+  const contents = fs.readFileSync(file, 'utf8')
+
+  const data = opts.templateContent.replace(/<%= url %>/g, url)
+    .replace(/<%= contents %>/g, strEsc(contents))
+
+  return data
+}
+
+/**
+ * Add Angular Module template
+ * and replace vars with strings
+ */
+function angularWrapper (data, opts) {
+  data = opts.templateHeader + data + opts.templateFooter
+
+  return data.replace(/<%= module %>/g, opts.moduleName)
+    .replace(/<%= standalone %>/g, (opts.standalone ? ', []' : ''))
+}
+
+/**
+ * Wrap all content in Module definition, if any
+ */
+function moduleWrapper (data, opts) {
+  const system = opts.moduleSystem
+  const module = modules[system]
+
+  if (!system || !module) {
+    return data;
   }
+
+  return (module.header || '') + data + (module.footer || '')
 }
