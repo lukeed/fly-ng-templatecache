96dbcee2e0d6d1d07c29898ed8d7f854b89f505c
Luke Edwards <luke.edwards05@gmail.com>
2016-10-22 19:59:19 +0200

    upgrade plugin logic

--- index.js
+++ index.js
@@ -1,87 +1,74 @@
-const fs = require('fs')
-const path = require('path')
-const assign = require('object-assign')
-const strEsc = require('js-string-escape')
+'use strict';
+
+const p = require('path');
+const strEsc = require('js-string-escape');
 
 const modules = {
-  requirejs: {
-    header: 'define([\'angular\'], function(angular) { \'use strict\'; return ',
-    footer: '});'
-  },
-  browserify: {
-    header: '\'use strict\'; module.exports = '
-  },
-  es6: {
-    header: 'import angular from \'angular\'; export default ',
-  },
-  iife: {
-    header: '(function(){',
-    footer: '})();'
-  }
-}
+	requirejs: {
+		header: 'define([\'angular\'], function(angular) { \'use strict\'; return ',
+		footer: '});'
+	},
+	browserify: {
+		header: '\'use strict\'; module.exports = '
+	},
+	es6: {
+		header: 'import angular from \'angular\'; export default '
+	},
+	iife: {
+		header: '(function(){',
+		footer: '})();'
+	}
+};
 
 const defaults = {
-  file: 'templates.js',
-  moduleName: 'templates',
-  moduleSystem: null,
-  standalone: false,
-  templateHeader: 'angular.module("<%= module %>"<%= standalone %>).run(["$templateCache", function($templateCache) {',
-  templateContent: '$templateCache.put("<%= url %>","<%= contents %>");',
-  templateFooter: '}]);',
-  transformUrl: (url) => url
-}
+	file: 'templates.js',
+	standalone: false,
+	moduleType: null,
+	moduleName: 'templates',
+	templateHeader: 'angular.module("<%= module %>"<%= standalone %>).run(["$templateCache", function($templateCache) {',
+	templateContent: '$templateCache.put("<%= url %>","<%= contents %>");',
+	templateFooter: '}]);',
+	trim: ''
+};
 
 module.exports = function () {
-  this.filter('ngTemplates', (data, opts) => {
-    opts = assign(defaults, opts)
+	this.plugin('ngTemplates', {every: 0}, function * (files, opts) {
+		opts = Object.assign({}, defaults, opts);
 
-    return this.unwrap(files =>
-      files.map(file => buildContent(file, opts)).join('')
-    ).then(data => {
-      data = angularWrapper(data, opts)
-      data = moduleWrapper(data, opts)
+		if (typeof opts.trim === 'string') {
+			const t = opts.trim;
+			opts.trim = str => str.replace(new RegExp(t, 'i'), '/');
+		}
 
-      this.concat(opts.file)
+		let data = files.map(file => {
+			// strip a string from the `file.dir` path
+			let dir = p.relative(this.root, file.dir);
+			// apply `opts.trim` func
+			dir = p.normalize(opts.trim(dir));
+			// esure no leading '/'
+			dir = dir.chartAt(0) === '/' ? dir.substr(1) : dir;
+			// wrap & return each file's content in a $templateCache definition
+			return opts.templateContent
+				.replace('<%= url %>', dir)
+				.replace('<%= contents %>', strEsc(file.data.toString()));
+		}).join('');
 
-      return data
-    })
-  })
-}
+		// "merge" all data && wrap with angular
+		data = opts.templateHeader.concat(data, opts.templateFooter)
+			.replace('<%= module %>', opts.moduleName)
+			.replace('<%= standalone %>', (opts.standalone ? ', []' : ''));
 
-/**
- * Wrap each file's contents in a $templateCache definition
- */
-function buildContent (file, opts) {
-  const url = opts.transformUrl( path.normalize(file) )
-  const contents = fs.readFileSync(file, 'utf8')
+		const mod = opts.moduleType && modules[opts.moduleType];
 
-  const data = opts.templateContent.replace(/<%= url %>/g, url)
-    .replace(/<%= contents %>/g, strEsc(contents))
+		if (mod) {
+			data = (mod.header || '').concat(data, mod.footer || '');
+		}
 
-  return data
-}
-
-/**
- * Add Angular Module template
- * and replace vars with strings
- */
-function angularWrapper (data, opts) {
-  data = opts.templateHeader + data + opts.templateFooter
-
-  return data.replace(/<%= module %>/g, opts.moduleName)
-    .replace(/<%= standalone %>/g, (opts.standalone ? ', []' : ''))
-}
-
-/**
- * Wrap all content in Module definition, if any
- */
-function moduleWrapper (data, opts) {
-  const system = opts.moduleSystem
-  const module = modules[system]
-
-  if (!system || !module) {
-    return data
-  }
-
-  return (module.header || '') + data + (module.footer || '')
-}
+		// clone file entry; keep `dir`
+		let out = files[0];
+		out.base = opts.file;
+		out.data = new Buffer(data);
+		// wipe ALL files in favor of new clone
+		this._.files = [out];
+	});
+};
